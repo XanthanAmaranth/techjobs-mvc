fbc42c6b33ffd43d42a461ee359e07c87898f4fe
XanthanAmaranth <xanthanamaranth@gmail.com>
2018-06-14 16:34:19 -0500

    techjobs-mvc completed version

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,23 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", jobs);
+            return "search";
+
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+            ListController ListContoller = null;
+            model.addAttribute("title", "Jobs with" + ListContoller.columnChoices.get(searchType) + "");
+            model.addAttribute("columns", ListContoller.columnChoices);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+    }
 }
